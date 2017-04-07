37edfa6c7bdd6efe1228b68ceac1e6029e1244b2
Shiva Thakkur <thakurshiva89@gmail.com>
2017-04-07 11:58:21 +0530

    Added privilage module

---
+++ privilage/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Privilage',
+    'version': '1.1',
+    'category': 'Privilage',
+    'description': """
+Privilage.
+    """,
+    'website': 'https://www.odoo.com/page/accounting',
+    'depends' : ['account'],
+    'data': ['account_view.xml' ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ privilage/readme.md
@@ -0,0 +1,5 @@
+steps to check
+
+1) Install module
+2) Go to users -> tick Master group at bottom of user group
+3) Now you can see chart of accounts menu means in accounting or Invoicing.
